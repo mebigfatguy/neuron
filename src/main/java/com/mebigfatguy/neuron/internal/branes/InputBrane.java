1f48effaa68cfcb4117c99f59fd7c222579f3670
Dave Brosius <dbrosius@mebigfatguy.com>
2018-01-29 16:56:24 -0500

    Happy New Year

--- src/main/java/com/mebigfatguy/neuron/internal/branes/InputBrane.java
+++ src/main/java/com/mebigfatguy/neuron/internal/branes/InputBrane.java
@@ -1,6 +1,6 @@
 /*
  * neuron - a neural network
- * Copyright (C) 2017 Dave Brosius
+ * Copyright (C) 2017-2018 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
